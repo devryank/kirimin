f8eed3ca37ef796bd022bf74355a976ccf466660
devryank <ryansg41@gmail.com>
2021-09-21 21:02:45 +0700

    add some migration

---
+++ database/migrations/2021_09_21_100044_create_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('rt');
+            $table->integer('rw');
+            $table->string('kecamatan');
+            $table->string('kelurahan');
+            $table->string('kota');
+            $table->string('provinsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
