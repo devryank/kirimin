abab25063fc0198d3a22ae0f2ad17e2ecb192b9b
devryank <ryansg41@gmail.com>
2021-09-23 18:12:43 +0700

    add detail for get specific data

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -17,4 +17,34 @@ class AddressController extends Controller
         $response = file_get_contents('https://dev.farizdotid.com/api/daerahindonesia/kecamatan?id_kota=' . $city_id);
         return $response;
     }
+
+    public function kelurahan($kecamatan_id)
+    {
+        $response = file_get_contents('https://dev.farizdotid.com/api/daerahindonesia/kelurahan?id_kecamatan=' . $kecamatan_id);
+        return $response;
+    }
+
+    public function province_detail($province_id)
+    {
+        $response = file_get_contents('https://dev.farizdotid.com/api/daerahindonesia/provinsi/' . $province_id);
+        return $response;
+    }
+
+    public function city_detail($city_id)
+    {
+        $response = file_get_contents('https://dev.farizdotid.com/api/daerahindonesia/kota/' . $city_id);
+        return $response;
+    }
+
+    public function kecamatan_detail($kecamatan_id)
+    {
+        $response = file_get_contents('https://dev.farizdotid.com/api/daerahindonesia/kecamatan/' . $kecamatan_id);
+        return $response;
+    }
+
+    public function kelurahan_detail($kelurahan_id)
+    {
+        $response = file_get_contents('https://dev.farizdotid.com/api/daerahindonesia/kelurahan/' . $kelurahan_id);
+        return $response;
+    }
 }
