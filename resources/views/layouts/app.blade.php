12a2e37b3a6619c3a0a0ce328ca44d44a0aae915
devryank <ryansg41@gmail.com>
2021-09-23 18:12:08 +0700

    add warung to sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,20 +3,15 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tailwind Admin Template</title>
-    <meta name="author"
-          content="David Grzyb">
-    <meta name="description"
-          content="">
+    <meta name="author" content="David Grzyb">
+    <meta name="description" content="">
 
     <!-- Tailwind -->
-    <link href="{{ mix('css/app.css') }}"
-          rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}"
-            defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <style>
         @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
 
@@ -60,23 +55,30 @@
 
     <aside class="relative bg-sidebar dark:bg-gray-700 h-screen w-64 hidden sm:block shadow-xl">
         <div class="p-6">
-            <a href="index.html"
-               class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Admin</a>
+            <a href="index.html" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Admin</a>
             <button
-                    class="w-full bg-white dark:bg-blue-600 dark:text-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 dark:hover:bg-blue-800 flex items-center justify-center">
+                class="w-full bg-white dark:bg-blue-600 dark:text-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 dark:hover:bg-blue-800 flex items-center justify-center">
                 <i class="fas fa-plus mr-3"></i> New Report
             </button>
         </div>
         <nav class="text-white text-base font-semibold pt-3">
             <a href="{{route('dashboard')}}"
-               class="flex items-center {{Request::segment(2) == '' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
+                class="flex items-center {{Request::segment(2) == '' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
                 <i class="fas fa-tachometer-alt mr-3"></i>
                 Dashboard
             </a>
 
+            @if (Auth::user()->hasPermissionTo('read shops'))
+            <a href="{{route('dashboard.shop.index')}}"
+                class="flex items-center {{Request::segment(2) == 'shops' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
+                <i class="fas fa-store mr-3"></i>
+                Warung
+            </a>
+            @endif
+
             @if (Auth::user()->hasPermissionTo('read users'))
             <a href="{{route('dashboard.user.index')}}"
-               class="flex items-center {{Request::segment(2) == 'users' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
+                class="flex items-center {{Request::segment(2) == 'users' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
                 <i class="fas fa-users mr-3"></i>
                 Users
             </a>
@@ -84,7 +86,7 @@
 
             @if (Auth::user()->hasPermissionTo('read roles'))
             <a href="{{route('dashboard.role.index')}}"
-               class="flex items-center {{Request::segment(2) == 'roles' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
+                class="flex items-center {{Request::segment(2) == 'roles' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
                 <i class="fas fa-layer-group mr-3"></i>
                 Roles
             </a>
@@ -96,31 +98,22 @@
         <!-- Desktop Header -->
         <header class="w-full items-center bg-white dark:bg-gray-800 py-2 px-6 hidden sm:flex">
             <div class="w-1/2"></div>
-            <div x-data="{ isOpen: false }"
-                 class="relative w-1/2 flex justify-end">
-                <div id="mode"
-                     class="pt-3 mr-3">
-                    <i id="modeIcon"
-                       class="fas fa-toggle-off fa-2x text-blue-600 dark:text-white cursor-pointer"></i>
+            <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
+                <div id="mode" class="pt-3 mr-3">
+                    <i id="modeIcon" class="fas fa-toggle-off fa-2x text-blue-600 dark:text-white cursor-pointer"></i>
                 </div>
                 <button @click="isOpen = !isOpen"
-                        class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-4 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
+                    class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-4 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
                     <img src="https://source.unsplash.com/uJ8LNVCBjFQ/400x400">
                 </button>
-                <button x-show="isOpen"
-                        @click="isOpen = false"
-                        class="h-full w-full fixed inset-0 cursor-default"></button>
-                <div x-show="isOpen"
-                     class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16 z-50">
-                    <a href="{{ route('logout') }}"
-                       class="block px-4 py-2 account-link hover:text-white"
-                       onclick="event.preventDefault();
+                <button x-show="isOpen" @click="isOpen = false"
+                    class="h-full w-full fixed inset-0 cursor-default"></button>
+                <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16 z-50">
+                    <a href="{{ route('logout') }}" class="block px-4 py-2 account-link hover:text-white" onclick="event.preventDefault();
                                     document.getElementById('logout-form').submit();">
                         Sign Out
                     </a>
-                    <form method="POST"
-                          id="logout-form"
-                          action="{{ route('logout') }}">
+                    <form method="POST" id="logout-form" action="{{ route('logout') }}">
                         @csrf
                     </form>
                 </div>
@@ -128,52 +121,42 @@
         </header>
 
         <!-- Mobile Header & Nav -->
-        <header x-data="{ isOpen: false }"
-                class="w-full bg-sidebar py-5 px-6 sm:hidden">
+        <header x-data="{ isOpen: false }" class="w-full bg-sidebar py-5 px-6 sm:hidden">
             <div class="flex items-center justify-between">
-                <a href="index.html"
-                   class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Admin</a>
-                <button @click="isOpen = !isOpen"
-                        class="text-white text-3xl focus:outline-none">
-                    <i x-show="!isOpen"
-                       class="fas fa-bars"></i>
-                    <i x-show="isOpen"
-                       class="fas fa-times"></i>
+                <a href="index.html" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Admin</a>
+                <button @click="isOpen = !isOpen" class="text-white text-3xl focus:outline-none">
+                    <i x-show="!isOpen" class="fas fa-bars"></i>
+                    <i x-show="isOpen" class="fas fa-times"></i>
                 </button>
             </div>
 
             <!-- Dropdown Nav -->
-            <nav :class="isOpen ? 'flex': 'hidden'"
-                 class="flex flex-col pt-4">
+            <nav :class="isOpen ? 'flex': 'hidden'" class="flex flex-col pt-4">
                 <a href="{{route('dashboard')}}"
-                   class="flex items-center {{Request::segment(2) == '' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
+                    class="flex items-center {{Request::segment(2) == '' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
                     <i class="fas fa-tachometer-alt mr-3"></i>
                     Dashboard
                 </a>
                 @if (Auth::user()->hasPermissionTo('read users'))
                 <a href="{{route('dashboard.user.index')}}"
-                   class="flex items-center {{Request::segment(2) == 'users' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
+                    class="flex items-center {{Request::segment(2) == 'users' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
                     <i class="fas fa-users mr-3"></i>
                     Users
                 </a>
                 @endif
                 @if (Auth::user()->hasPermissionTo('read roles'))
                 <a href="{{route('dashboard.role.index')}}"
-                   class="flex items-center {{Request::segment(2) == 'roles' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
+                    class="flex items-center {{Request::segment(2) == 'roles' ? 'active-nav-link' : ''}} text-white py-4 pl-6 nav-item">
                     <i class="fas fa-layer-group mr-3"></i>
                     Roles
                 </a>
                 @endif
-                <a href="{{ route('logout') }}"
-                   class="flex items-center text-white py-4 pl-6 nav-item"
-                   onclick="event.preventDefault();
+                <a href="{{ route('logout') }}" class="flex items-center text-white py-4 pl-6 nav-item" onclick="event.preventDefault();
                                        document.getElementById('logout-form').submit();">
                     <i class="fas fa-sign-out-alt mr-3"></i>
                     Sign Out
                 </a>
-                <form method="POST"
-                      id="logout-form"
-                      action="{{ route('logout') }}">
+                <form method="POST" id="logout-form" action="{{ route('logout') }}">
                     @csrf
                 </form>
             </nav>
@@ -188,21 +171,17 @@
             </main>
 
             <footer class="w-full bg-white dark:bg-gray-800 dark:text-white text-right p-4">
-                Built by <a target="_blank"
-                   href="https://davidgrzyb.com"
-                   class="underline">David Grzyb</a>.
+                Built by <a target="_blank" href="https://davidgrzyb.com" class="underline">David Grzyb</a>.
             </footer>
         </div>
 
     </div>
 
     <!-- AlpineJS -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
-            defer></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
-            integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs="
-            crossorigin="anonymous"></script>
+        integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
     <script src="{{asset('js/darkmode.js')}}"></script>
 
     <script>
