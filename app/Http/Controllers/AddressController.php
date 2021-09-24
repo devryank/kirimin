a3838361c62fee02fa846596b2047953c7a38c6b
devryank <ryansg41@gmail.com>
2021-09-24 15:04:19 +0700

    add list province

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class AddressController extends Controller
 {
+    public function province()
+    {
+        $response = file_get_contents('https://dev.farizdotid.com/api/daerahindonesia/provinsi');
+        return $response;
+    }
+
     public function city($province_id)
     {
         $response = file_get_contents('https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=' . $province_id);
