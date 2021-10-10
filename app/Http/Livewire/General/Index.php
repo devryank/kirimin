bf9fd60aef2932616674590e39a17d0efbbf3786
devryank <ryansg41@gmail.com>
2021-10-10 17:43:07 +0700

    add pagination, filter address and design

--- app/Http/Livewire/General/Index.php
+++ app/Http/Livewire/General/Index.php
@@ -3,12 +3,20 @@
 namespace App\Http\Livewire\General;
 
 use App\Models\Shop;
+use App\Models\Address;
 use Livewire\Component;
+use Livewire\WithPagination;
 use Illuminate\Support\Facades\Auth;
 
 class Index extends Component
 {
-    public $qty = 0;
+    use WithPagination;
+
+    public $search;
+    public $paginate = 10;
+
+    public $selectAddress = 'rw';
+
     public function mount()
     {
         if (!Auth::user()) {
@@ -20,15 +28,38 @@ class Index extends Component
 
     public function render()
     {
-        $shops = Shop::all();
+        $userAddress = json_decode(Auth::user()->address);
+        if ($this->selectAddress == 'rt') {
+            $addresses = Address::where([
+                ['rt', '=', $userAddress->rt],
+                ['rw', '=', $userAddress->rw],
+                ['kelurahan', '=', $userAddress->kelurahan],
+            ])->get();
+        }
+        if ($this->selectAddress == 'rw') {
+            $addresses = Address::where([
+                ['rw', '=', $userAddress->rw],
+                ['kelurahan', '=', $userAddress->kelurahan],
+            ])->get();
+        }
+        if ($this->selectAddress == 'kelurahan') {
+            $addresses = Address::where([
+                ['kelurahan', '=', $userAddress->kelurahan],
+            ])->get();
+        }
+
+        $addressId = array();
+        foreach ($addresses as $key => $address) {
+            $addressId[] = $address->id;
+        }
+
+        if (!empty($addressId)) {
+            $shops = Shop::whereIn('address_id', $addressId)->paginate($this->paginate);
+        } else {
+            $shops = [];
+        }
         return view('livewire.general.index', [
             'shops' => $shops
         ])->extends('layouts.general')->section('content');
-    }
-
-    public function createOrder()
-    {
-        // $this->addQty = true;
-        $this->qty = $this->qty + 1;
     }
 }
--- resources/views/livewire/general/index.blade.php
+++ resources/views/livewire/general/index.blade.php
@@ -1,6 +1,26 @@
+@push('css')
+<style>
+    #pagination nav p {
+        margin-right: 20px;
+    }
+</style>
+@endpush
 <div>
-    <section class="ml-4 py-3">
+    <section class="mx-4 py-3">
         <div class="grid grid-cols-12 gap-4">
+            <div class="col-span-12">
+                Cari toko di lingkungan:
+                <select class="w-32" wire:model="selectAddress">
+                    <option value="rt">RT</option>
+                    <option value="rw">RW</option>
+                    <option value="kelurahan">Kelurahan</option>
+                </select>
+            </div>
+            @if (empty($shops))
+            <div class="col-span-12">
+                Toko tidak ditemukan
+            </div>
+            @endif
             @foreach ($shops as $shop)
             <a href="{{ route('general.show', $shop->id) }}"
                 class="col-span-3 max-w-sm rounded overflow-hidden shadow-lg">
@@ -13,6 +33,11 @@
                 </div>
             </a>
             @endforeach
+        </div>
+        <div class="grid grid-cols-12 gap-4 my-8">
+            <div class="col-span-12 mx-auto" id="pagination">
+                {{$shops->links()}}
+            </div>
         </div>
     </section>
 </div>
