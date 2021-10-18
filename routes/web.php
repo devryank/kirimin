eccd73fe75140d33b4fce1e60d7dd51e70b75527
devryank <ryansg41@gmail.com>
2021-10-18 15:56:03 +0700

    add controller to dashboard route

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
 Route::name('dashboard.')
     ->prefix('dashboard')
