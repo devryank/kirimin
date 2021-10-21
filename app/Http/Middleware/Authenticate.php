04de31293a1af2cf5c6b229bd2ecd758d2b5f8f6
devryank <ryansg41@gmail.com>
2021-10-21 12:09:42 +0700

    move unauthenticate redirect to login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (!$request->expectsJson()) {
-            return route('noauth');
+            return route('login');
         }
     }
 }
